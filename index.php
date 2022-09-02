bb9836bd2c86bc9bb9f2d3ec10a3a02489ed7b43
rappadapa <unfortunatelyzebras@gmail.com>
2022-09-01 23:33:54 -0400

    died (tried to fix navbar and failed)

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+
+
+ ?>
+
+
+ <!DOCTYPE html>
+ <html>
+ <!--head-->
+ <head>
+   <meta charset="utf-8">
+   <title> FeralHeart Unleashed </title>
+  <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
+
+   <link rel="stylesheet" href="css/index.css">
+   <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Pangolin&family=Roboto+Condensed&display=swap" rel="stylesheet">
+ </head>
+
+<!--body-->
+<header>
+  <nav>
+    <div class="navlogo">
+      <a href="index.php"> <img src="img/LOGO_WIP.png" alt="logo"> </a>
+    </div>
+    <ul>
+      <li> <a href="#"> Home </a> </li>
+      <li> <a href="#"> News </a></li>
+      <li> <a href="#"> Download </a> </li>
+      <li>
+        <a href="#"> Rules </a>
+        <ul>
+          <li> <a href="#"> Private Policy </a> </li>
+          <li> <a href="#"> Terms of Service </a> </li>
+        </ul>
+      </li>
+      <li>
+        <a href="#"> Support </a>
+          <ul id="supportlinks">
+            <li> <a href="#"> Tickets </a> </li>
+            <li> <a href="#"> Reports </a> </li>
+            <li> <a href="#"> Wiki </a> </li>
+          </ul>
+      </li>
+      <li>
+        <a href="#"> User Panel </a>
+          <ul id="userlinks">
+            <li> <a href="#"> Login </a> </li>
+            <li> <a href="#"> Register </a> </li>
+          </ul>
+      </li>
+    </ul>
+  </nav>
+</header>
+
+<!--body-->
+<body>
+
+
+
+<div class="body-wrapper">
+  <!--add a hero? up above the title -->
+  <div class="hero-slideshow">
+
+  </div>
+</div>
+<!-- end "body-wrapper" -->
--- js/test.js
+++
@@ -1,8 +0,0 @@
-
-
-
-
-//roadmap button
-document.getElementById("roadmap").addEventListener("click", function() {
- location.href = 'boiler.html';
-});
